492ca45af30eaef5fe0306d0cec784bc72bc5004
zahid-official <zahid.official8@gmail.com>
2026-06-14 19:40:53 +0600

    handle redirect in route handlers

--- code-evulation/src/app/(server)/profile/api/route.ts
+++ code-evulation/src/app/(server)/profile/api/route.ts
@@ -1,25 +1,31 @@
 import { cookies, headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 // GET Function
 const getProfileApi = async () => {
-  // headers
+  // Headers
   const headersList = (await headers()).get("Authorization")?.split(" ");
   if (!headersList) {
     return new Response("Authentication required", { status: 401 });
   }
   const headersToken = headersList[1];
 
-  // cookies
+  // Cookies
   const cookieStore = await cookies();
   cookieStore.set("theme", "dark"); // set cookie using cookies method
   const cookieTheme = cookieStore.get("theme")?.value;
   console.log({ cookieTheme });
 
-  // cookieStore.delete("authToken");
+  // Redirect if cookie is not present
   const checkCookie = cookieStore.has("authToken");
   console.log(checkCookie);
+  if (!checkCookie) {
+    redirect("/");
+  }
 
-  // response
+  // cookieStore.delete("authToken");
+
+  // Response
   return new Response(`<h1>Profile API Data</h1>`, {
     headers: {
       "Content-Type": "text/html",
