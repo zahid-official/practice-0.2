99f8a19c245e9181635a990fa27f5d4e2179d8d2
zahid-official <zahid.official8@gmail.com>
2026-06-14 18:45:40 +0600

    handle headers & cookies in route handlers

--- code-evulation/src/app/(server)/profile/api/route.ts
+++ code-evulation/src/app/(server)/profile/api/route.ts
@@ -1,7 +1,31 @@
+import { cookies, headers } from "next/headers";
+
 // GET Function
-const getProfileAPI = async () => {
-  // Return a JSON response
-  return new Response("Profile API Data");
+const getProfileApi = async () => {
+  // headers
+  const headersList = (await headers()).get("Authorization")?.split(" ");
+  if (!headersList) {
+    return new Response("Authentication required", { status: 401 });
+  }
+  const headersToken = headersList[1];
+
+  // cookies
+  const cookieStore = await cookies();
+  cookieStore.set("theme", "dark"); // set cookie using cookies method
+  const cookieTheme = cookieStore.get("theme")?.value;
+  console.log({ cookieTheme });
+
+  // cookieStore.delete("authToken");
+  const checkCookie = cookieStore.has("authToken");
+  console.log(checkCookie);
+
+  // response
+  return new Response(`<h1>Profile API Data</h1>`, {
+    headers: {
+      "Content-Type": "text/html",
+      "Set-Cookie": `authToken=${headersToken}`, // set cookie using headers response
+    },
+  });
 };
 
-export { getProfileAPI as GET };
+export { getProfileApi as GET };
