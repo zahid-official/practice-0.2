2f13695b5b6af4ae7fb28658328af5c1949cf259
zahid-official <zahid.official8@gmail.com>
2026-06-21 21:01:46 +0600

    config prisma with sqlite

--- code-evulation/src/app/fetching/database/page.tsx
+++ code-evulation/src/app/fetching/database/page.tsx
@@ -1,8 +1,27 @@
+import { getAllProducts } from "@/lib/db-operations";
+import { Product } from "../../../../generated/prisma/client";
+
 // DatabasePage Component
-const DatabasePage = () => {
+const DatabasePage = async () => {
+  const products: Product[] = await getAllProducts();
+
   return (
     <div>
-      <h1>Welcome to the DatabasePage Component</h1>
+      <h1 className="">Welcome to the DatabasePage Component</h1>
+
+      <h1 className="text-4xl underline underline-offset-4 font-semibold mt-8 text-center">
+        All Products - {products.length}
+      </h1>
+
+      <ul className="grid grid-cols-1 gap-4 md:grid-cols-2 list-decimal list-inside space-y-4 mx-8 py-5">
+        {products.map((product) => (
+          <li key={product.id} className="space-y-2 border p-4">
+            <h2>Title: {product.title}</h2>
+            <p>Description: {product.description}</p>
+            <p>Price: ${product.price}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
---
+++ code-evulation/src/lib/db-operations.ts
@@ -0,0 +1,43 @@
+import { prisma } from "./prisma";
+
+// Seed the database with products if there are no products
+const seedProducts = async () => {
+  const count = await prisma.product.count();
+  if (!count) {
+    await prisma.product.createMany({
+      data: [
+        {
+          title: "Premium Wireless Headphones",
+          description:
+            "Experience studio-quality sound with active noise cancellation and 40-hour battery life.",
+          price: 199,
+        },
+        {
+          title: "Minimalist Leather Wallet",
+          description:
+            "Sleek RFID-blocking cardholder crafted from premium full-grain leather.",
+          price: 49,
+        },
+        {
+          title: "Ergonomic Mechanical Keyboard",
+          description:
+            "Tactile typing experience with hot-swappable switches and customizable RGB backlighting.",
+          price: 129,
+        },
+      ],
+    });
+  }
+};
+
+try {
+  await seedProducts();
+} catch (error) {
+  console.error("Error seeding products:", error);
+}
+
+// getAllProducts function
+const getAllProducts = async () => {
+  return await prisma.product.findMany();
+};
+
+export { getAllProducts };
