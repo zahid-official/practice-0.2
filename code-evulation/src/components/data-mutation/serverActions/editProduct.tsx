618b161d2ffe1401174ceba30df08606dfcf0144
zahid-official <zahid.official8@gmail.com>
2026-06-23 22:55:01 +0600

    create edit product server action

---
+++ code-evulation/src/components/data-mutation/serverActions/editProduct.tsx
@@ -0,0 +1,10 @@
+// editProduct Component
+const editProduct = () => {
+  return (
+    <div>
+      <h1>Welcome to the editProduct Component</h1>
+    </div>
+  );
+};
+
+export default editProduct;
