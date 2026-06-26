ec52fba791fc12c256047578ebf3675466afb070
zahid-official <zahid.official8@gmail.com>
2026-06-26 08:56:46 +0600

    manage user roles by admin

--- code-evulation/src/app/(rbac)/admin/page.tsx
+++ code-evulation/src/app/(rbac)/admin/page.tsx
@@ -1,8 +1,66 @@
+import { Button } from "@/components/ui/button";
+import { clerkClient } from "@clerk/nextjs/server";
+import Form from "next/form";
+import { Roles } from "../../../../types/globals";
+import { makeAdmin, removeAdmin } from "./changeRole";
+
 // AdminPage Component
-const AdminPage = () => {
+const AdminPage = async () => {
+  const client = await clerkClient();
+  const users = (await client.users.getUserList()).data;
+
   return (
-    <div>
-      <h1>Admin only page</h1>
+    <div className="py-10 space-y-10">
+      <h1 className="text-2xl font-semibold underline text-center">
+        Admin Panel
+      </h1>
+
+      <div>
+        {users?.map((user, idx) => {
+          const changeRole = makeAdmin.bind(null, {
+            id: user.id,
+            role: "ADMIN",
+          });
+
+          const removeRole = removeAdmin.bind(null, {
+            id: user.id,
+            role: "MODERATOR",
+          });
+
+          return (
+            <div
+              key={user.id}
+              className="flex justify-between items-center gap-4 container mx-auto border border-gray-200 p-5"
+            >
+              {/* User Info */}
+              <div className="flex gap-10">
+                <p>
+                  {idx + 1}. Name: {user.firstName} {user.lastName}
+                </p>
+                <p>Email: {user.emailAddresses[0].emailAddress}</p>
+                <p>Role: {user.publicMetadata.role as Roles}</p>
+              </div>
+
+              {/* Actions */}
+              <div>
+                <Form
+                  action={
+                    user.publicMetadata.role === "ADMIN"
+                      ? removeRole
+                      : changeRole
+                  }
+                >
+                  <Button type="submit">
+                    {user.publicMetadata.role === "ADMIN"
+                      ? "Remove Role"
+                      : "Make Role Admin"}
+                  </Button>
+                </Form>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
