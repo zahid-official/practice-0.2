4b9919d059ed44bdb6ae703d760e55f0066494b8
zahid-official <zahid.official8@gmail.com>
2026-06-26 09:03:45 +0600

    reduce the duplicate codes from server action

--- code-evulation/src/app/(rbac)/admin/changeRole.ts
+++ code-evulation/src/app/(rbac)/admin/changeRole.ts
@@ -3,8 +3,8 @@
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 
-// makeAdmin serverAction
-const makeAdmin = async ({ id, role }: { id: string; role: string }) => {
+// updateUserRole Helper Function
+const updateUserRole = async (id: string, role: string) => {
   const { sessionClaims, userId } = await auth();
   if (userId === id) {
     throw new Error("You are not authorized to change your own role.");
@@ -22,37 +22,22 @@ const makeAdmin = async ({ id, role }: { id: string; role: string }) => {
       },
     });
     revalidatePath("/admin");
-    console.log("Role changed successfully");
   } catch (error) {
     console.log("Error changing role:", error);
     throw error;
   }
 };
 
+// makeAdmin serverAction
+const makeAdmin = async ({ id, role }: { id: string; role: string }) => {
+  await updateUserRole(id, role);
+  console.log("Role changed successfully");
+};
+
 // removeAdmin serverAction
 const removeAdmin = async ({ id, role }: { id: string; role: string }) => {
-  const { sessionClaims, userId } = await auth();
-  if (userId === id) {
-    throw new Error("You are not authorized to change your own role.");
-  }
-
-  if (sessionClaims?.metadata?.role !== "ADMIN") {
-    throw new Error("You are not authorized to change the role.");
-  }
-
-  const client = await clerkClient();
-  try {
-    await client.users.updateUser(id, {
-      publicMetadata: {
-        role: role,
-      },
-    });
-    revalidatePath("/admin");
-    console.log("Role removed successfully");
-  } catch (error) {
-    console.log("Error removing role:", error);
-    throw error;
-  }
+  await updateUserRole(id, role);
+  console.log("Role removed successfully");
 };
 
 export { makeAdmin, removeAdmin };
